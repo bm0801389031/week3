11524d14d998d23924d8bdb11856d20443425119
Brian Monge <bm0801389031@gmail.com>
2021-01-18 16:41:19 -0700

    set up of cockroach problem

---
+++ cockroach.js
@@ -0,0 +1,41 @@
+/*
+The cockroach is one of the fastest insects. 
+Write a function which takes its speed in km per 
+hour and returns it in cm per second, rounded down 
+to the integer (= floored).
+
+For example:
+
+cockroachSpeed(1.08) == 30
+Note! The input is a Real number (actual type is 
+language dependent) and is >= 0. The result should 
+be an Integer.
+FUNDAMENTALS
+*/
+
+function cockroachSpeed(s) {
+    //Good Luck!
+  }
+
+
+
+
+  
+/*
+Test.describe("Basic Tests", function() {
+  Test.assertEquals(cockroachSpeed(1.08), 30);
+  Test.assertEquals(cockroachSpeed(1.09), 30);
+  Test.assertEquals(cockroachSpeed(0), 0);
+});
+
+var ans = (x) => parseInt(x / 0.036);
+Test.describe("Random Tests", function() {
+  for (let i = 0; i < 500; ++i) {
+    let s = Math.random() + parseInt(Math.random()*4);
+    
+    Test.it(`s = ${s}`, function() {
+      Test.assertEquals(cockroachSpeed(s), ans(s));
+    });
+  }
+});
+ */
