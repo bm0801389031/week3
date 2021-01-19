c9c20ff7cd86fa1fc40712b2d205dd79c996aee0
Brian Monge <bm0801389031@gmail.com>
2021-01-19 11:56:28 -0700

    first and last

---
+++ removefirstandlast.js
@@ -0,0 +1,58 @@
+/*
+Description:
+
+It's pretty straightforward. Your goal is to create a function
+that removes the first and last characters of a string. You're
+given one parameter, the original string. You don't have to worry 
+with strings with less than two characters.
+FUNDAMENTALS, BASIC LANGUAGE FEATURES, STRINGS
+*/
+
+
+
+
+
+
+
+
+function removeChar(str) {
+//code here
+}
+
+
+
+
+/*
+Test.describe("Tests", function(){
+
+Test.assertEquals(removeChar('eloquent'), 'loquen');
+Test.assertEquals(removeChar('country'), 'ountr');
+Test.assertEquals(removeChar('person'), 'erso');
+Test.assertEquals(removeChar('place'), 'lac');
+Test.assertEquals(removeChar('ooopsss'), 'oopss');
+
+});
+
+
+ 
+Test.describe("Random Tests", function(){
+  
+  function testSolution(str){
+        return this.str = str.substring(1, str.length-1);      
+  }
+  
+   for(var i = 0; i < 6; i++){
+    var possible = "abc123",
+        text = "";
+     for(var k = 0; k < 7; k++){
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+     }
+    Test.assertEquals(removeChar(text), testSolution(text));
+  }
+  
+}); 
+*/
+
+
+
+
