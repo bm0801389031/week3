46e0e507ea638f3d8880a4f5dbc0636c7e9f125c
Brian Monge <bm0801389031@gmail.com>
2021-01-22 10:38:57 -0700

    complete problem

---
+++ reversedstrings.js
@@ -0,0 +1,27 @@
+
+//set function statement to reverse with parameter/argument s
+function reverse(s){
+/*return parameter s with method split(), to split the string and 
+puts these substrings into an array; for the parameter, only 
+quotes with no space, that way it will pull each word in to an
+array.........
+then reverse() method which reverses an array in place, the first
+array element becomes the last and the last array element becomes
+the first .reverse holds nothing in the parantheses....
+then the join() method which creates and returns a new string by
+concatenating all of the elements in an array.. set with parameter
+with only quotations, no space, so it joins the array in to a whole 
+string*/
+	return s.split("").reverse().join("");
+}
+
+
+
+
+
+/*
+Test.assertEquals(solution('world'), 'dlrow')
+Test.assertEquals(solution('hello'), 'olleh')
+Test.assertEquals(solution(''), '')
+Test.assertEquals(solution('h'), 'h')
+*/
