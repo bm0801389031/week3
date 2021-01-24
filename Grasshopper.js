63d46396bd93fb493979d7e76cc00483b841a8f0
Brian Monge <bm0801389031@gmail.com>
2021-01-24 15:23:12 -0700

    complete file

---
+++ Grasshopper.js
@@ -0,0 +1,46 @@
+/*set function expression variable summation equals function with
+parameter/argument num */
+var summation = function (num) {
+/* let result equal 0*/
+  let result = 0;
+/* for loop with three expressions. first expression is a variable
+equal to 1; second expression is i less than or equal to num;
+third expression is i plus plus, so that i will incroment by one 
+through each loop iteration*/
+  for (var i = 1; i <= num; i++) {
+//result plus equals i
+    result += i;
+  }
+// return result
+  return result;
+}
+
+
+
+
+//test code
+
+describe('summation', function () {
+  it('should return the correct total', function () {
+    Test.assertEquals(summation(1), 1)
+    Test.assertEquals(summation(8), 36)
+    Test.assertEquals(summation(22), 253)
+    Test.assertEquals(summation(100), 5050)
+    Test.assertEquals(summation(213), 22791)
+  })
+  it('random numbers test', function () {
+    for (var i = 0; i < 100; i++) {
+      var rand = Test.randomNumber() + 1
+      Test.assertEquals(summation(rand), solution(rand))
+    }
+  })
+  function solution (num) {
+    var total = 0
+    for (var i = 1; i <= num; i++) {
+      total += i
+    }
+    return total
+  }
+})
+
+
